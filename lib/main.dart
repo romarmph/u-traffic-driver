64be9571b9fb7cc1a59115b593cfeeb72908c976
aqebytaurus <qwerty.drawde@yahoo.com>
2023-07-31 13:28:30 +0800

    fixed(july31)

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
-import 'package:flutter/material.dart';
-
-import 'package:firebase_core/firebase_core.dart';
-=======
 import 'package:driverslogin/screen/home.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
->>>>>>> 624b808 (fixed(july31))
 
 import 'firebase_options.dart';
 
@@ -23,20 +17,10 @@ class UTrafficDriver extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-<<<<<<< HEAD
-    return const MaterialApp(
-      title: "U-Traffic Driver",
-      home: Scaffold(
-        body: Center(
-          child: Text("Driver Home"),
-        ),
-      ),
-=======
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "U-Traffic Driver",
       home: homeScreen(),
->>>>>>> 624b808 (fixed(july31))
     );
   }
 }
